0e0bf5db746416dbdd368803624324206de0f2e1
iiSurf <nicksandberg@hotmail.com>
2026-08-02 15:07:14 -0700

    added use method for hero to use an item from inventory

--- Source/Model/Item.cs
+++ Source/Model/Item.cs
@@ -1,22 +1,22 @@
-// Project: TCSS 360 Dungeon Adventure
-// File: Item.cs
-// Team: Jadon Bennett, Joanna Duran, Nick Humeniuk-Sandberg, Sean Prigge
+/*
+ * TCSS 360 Dungeon Adventure
+ * Item.cs
+ * Team: Jadon Bennett, Joanna Duran, Nick Humeniuk-Sandberg, Sean Prigge
+ */
 
 namespace DungeonDelver.Source.Model
 {
     /// <summary>
-    /// Abstract base class for all items that can be collected or used
-    /// in the dungeon, including potions and pillars.
+    /// Abstract base class for all collectible items. Every item has a name and
+    /// applies some effect to a hero when used.
     /// </summary>
     public abstract class Item
     {
-        /// <summary>
-        /// The display name of this item.
-        /// </summary>
+        /// <summary>The display name of this item.</summary>
         private readonly string myName;
 
         /// <summary>
-        /// Initializes a new Item with the specified name.
+        /// Initializes a new Item with the given display name.
         /// </summary>
         /// <param name="theName">The display name of this item.</param>
         protected Item(string theName)
@@ -24,11 +24,14 @@ namespace DungeonDelver.Source.Model
             myName = theName;
         }
 
-        /// <summary>
-        /// The display name of this item.
-        /// </summary>
+        /// <summary>The display name of this item.</summary>
         public string Name => myName;
 
-        // Future: Use() method to be defined for consumable items
+        /// <summary>
+        /// Applies this item's effect to the given hero.
+        /// </summary>
+        /// <param name="theHero">The hero using the item.</param>
+        /// <returns>A description of the item's effect for display.</returns>
+        public abstract string Use(Hero theHero);
     }
 }
